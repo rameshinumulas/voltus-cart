25b2b1bdaae8c84f03f98a23f886421224924675
ramesh inumula <inumularamesh@gmail.com>
2025-06-06 22:36:26 +0530

    Change for Routing process initiated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useState } from 'react'
 import { CssBaseline } from "@mui/material";
-import LoginForm from './components/LoginForm';
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Login from "./pages/Login";
+import GoogleLogin from './pages/GoogleLogin';
+import Signup from './pages/Signup';
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  // const location = useLocation();
   return (
     <>
       <CssBaseline />
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login setAuth={setAuth} />} />
+          <Route path="/googleLogin" element={<GoogleLogin setAuth={setAuth}  />} />
+          <Route path="/signup" element={<Signup setAuth={setAuth}  />} />
+        </Routes>
+      </Router>
     </>
   );
 }
