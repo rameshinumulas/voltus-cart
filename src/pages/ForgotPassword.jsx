5ce77a5df1179e5104bb05fb43ee16639617f2d2
ramesh inumula <inumularamesh@gmail.com>
2025-06-08 11:04:02 +0530

    unused code removed

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,5 @@
 import { Container, Typography, Box } from "@mui/material";
 import styled from "@emotion/styled";
-import LoginForm from "../components/LoginForm";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import ForgetPasswordForm from "../components/ForgetPasswordForm";
